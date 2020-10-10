7ed3a96abf79116337d64af56615891c8d49ab98
Vasyl Kobil <vasylkobil@gmail.com>
2020-10-10 13:15:11 +0200

    Updates

---
+++ precache-manifest.56d01bd2f3dc60077e48edaffee72d9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f2b633300703d5974cf72174ff3f8b",
+    "url": "/Jammming/index.html"
+  },
+  {
+    "revision": "4ff60fe66036159672a7",
+    "url": "/Jammming/static/css/main.f49a0d00.chunk.css"
+  },
+  {
+    "revision": "3c1231c673518c3576f8",
+    "url": "/Jammming/static/js/2.77d68393.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jammming/static/js/2.77d68393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff60fe66036159672a7",
+    "url": "/Jammming/static/js/main.0a9259ff.chunk.js"
+  },
+  {
+    "revision": "7e7a0a17cc65b1c77cbd",
+    "url": "/Jammming/static/js/runtime-main.c8a79d7b.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/Jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
